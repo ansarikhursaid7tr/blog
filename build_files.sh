34874a035c6372e9dc69595de6622ad921e63ea3
ansarikhursaid7tr <er.khursaid7tr@gmail.com>
2024-09-22 16:07:31 -0500

    Initial Commit

---
+++ build_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Building project packages..."
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+
+echo "Collecting static files..."
+#python3 manage.py collectstatic --no-input
+
+echo "Migrating Database..."
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+echo "Build script completed successfully"
