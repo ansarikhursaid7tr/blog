47f2fee4fa2041b1df49f5134c9f10de96b7b8b6
ansarikhursaid7tr <er.khursaid7tr@gmail.com>
2024-09-22 19:09:31 -0500

    Initial Commit

--- build_files.sh
+++ build_files.sh
@@ -2,7 +2,7 @@
 
 echo "Building project packages..."
 python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements-dev.txt
+python3 -m pip install -r requirements.txt
 
 echo "Collecting static files..."
 python3 manage.py collectstatic --no-input
