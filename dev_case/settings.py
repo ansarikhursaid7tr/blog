e0e1e488d133216384e9ffa2ce14465fdf68878d
ansarikhursaid7tr <er.khursaid7tr@gmail.com>
2024-09-22 19:16:21 -0500

    Initial Commit

--- dev_case/settings.py
+++ dev_case/settings.py
@@ -179,7 +179,7 @@ USE_TZ = True
 
 # Static files & Media
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (str(BASE_DIR.joinpath("static")),)
+STATICFILES_DIRS = (str(BASE_DIR.joinpath("static")))
 STATIC_ROOT = str(BASE_DIR.joinpath("staticfiles"))
 MEDIA_ROOT = str(BASE_DIR.joinpath("media"))
 
